19219a08086bce9b6a258c282e4ca9aa69795aaa
nihatkhidirov <nihatmkh@code.edu.az>
2024-06-27 22:16:12 +0400

    First Commit

---
+++ ViewStartLayoutViewImport/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewStartLayoutViewImport.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            //ViewData, ViewBag, TempData
+            ViewData["name"] = "Filankes";
+            ViewBag.age = 20;
+            TempData["address"] = "Sumgayit";
+            //Index-den About-un View-na Data Gondermek (TempData ile)
+            int sngid = 10;
+            TempData["ID"] = sngid;
+            return RedirectToAction("About");
+        }
+        public IActionResult About()
+        {
+            int id = Convert.ToInt32(TempData["ID"]);
+
+            return View();
+        }
+    }
+}
